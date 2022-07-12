64fa75c2313961e3bf32f9cef5617373c8590923
Adam Kowalewski <akowalewski@deloittece.com>
2022-07-12 16:12:42 +0200

    Add Package

---
+++ src/com/pack/Main.java
@@ -0,0 +1,11 @@
+package com.pack;
+
+public class Main extends Menu{
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.start();
+
+
+    }
+}
---
+++ src/com/pack/Menu.java
@@ -0,0 +1,21 @@
+package com.pack;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private int choice = 0;
+
+    public void start(){
+        System.out.println("1.Graj\n2.Dodaj Hasło do listy");
+        System.out.print("Twój wybór: ");
+        Scanner scanner = new Scanner(System.in);
+        this.choice = scanner.nextInt();
+
+    }
+
+    protected Integer getChoice(){
+        return this.choice;
+    }
+
+}
